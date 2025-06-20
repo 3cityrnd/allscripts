2b34dbef0447c1d8421df39f9f27f8cf0539c439
Pawel Piotrowicz <3cityrnd@gmail.com>
2025-06-20 16:48:24 +0200

    update

--- utils/npu_check.py
+++ utils/npu_check.py
@@ -1,9 +1,11 @@
 import torch
+import torch_npu
+
 
 dev_string="npu"
 
 a=torch.rand(2,4,device=dev_string)
 b=torch.randn(2,2).npu()
 
-print(f'Tensor has been created on {dev_string}')
+print(f'[ok] npu is working : tensor has been created on {dev_string}')
 
