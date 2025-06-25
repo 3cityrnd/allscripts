7384c3d37fb2e1355b76166595203194fe7c7ddc
juniorkowal <juniorkowal123@gmail.com>
2025-06-25 11:24:11 +0200

    update dependencies

--- utils/prepare_evn_aarch64.sh
+++ utils/prepare_evn_aarch64.sh
@@ -5,7 +5,7 @@ conda create -n pyt311 python=3.11 && conda activate pyt311
 
 pip3 install attrs cython numpy==1.24.0 decorator sympy cffi pyyaml pathlib2 psutil protobuf==3.20 scipy requests absl-py cloudpickle ml-dtypes tornado
 
-pip3 install torch==2.1.0 pyyaml setuptools torch-npu==2.1.0.post12 torch-tb-profiler-ascend cloudpickle ml-dtypes tornado
+pip3 install torch==2.1.0 pyyaml setuptools torch-npu==2.1.0.post12 torch-tb-profiler-ascend
 
 
 #pip3 install torch==2.6.0
